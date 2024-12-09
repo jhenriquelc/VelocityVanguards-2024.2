15ff5600158f07fa7a5f65a9d8bc0d769ddeb8d4
João H. L. Corrêa <jhenriquelc@gmail.com>
2024-12-09 09:43:04 -0300

    Adicionar protótipo de landing page
    Inclui:
    * Adição de componente VvHero
    * Adição de imagem de fundo para a landing page
    * Alterações no esqueleto da langing page

--- Site/src/routes/+page.svelte
+++ Site/src/routes/+page.svelte
@@ -1,11 +1,8 @@
 <script>
-	import { P, Heading } from 'flowbite-svelte';
 	import VvHeader from '$lib/VvHeader.svelte';
+	import VvHero from './VvHero.svelte';
 </script>
 
 <VvHeader></VvHeader>
 
-<div class="m-8">
-	<Heading class="mb-4 text-center">Avance na vida.</Heading>
-	<P>Descubra como a Vanguard lhe põe à frente do mercado imobiliário.</P>
-</div>
+<VvHero></VvHero>
