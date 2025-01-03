d73ba508e5f4fd3e9c7a0c5c39657382790e5b4d
João H. L. Corrêa <jhenriquelc@gmail.com>
2025-01-03 11:57:18 -0300

    Remover snippets trazidos acidentalmente ao trazer alterações da main ao #52

--- Site/src/routes/+page.svelte
+++ Site/src/routes/+page.svelte
@@ -1,15 +1,9 @@
 <script>
-	import { Heading, P } from 'flowbite-svelte';
 	import VvHeader from '$lib/VvHeader.svelte';
 	import VvFooter from '$lib/VvFooter.svelte';
 	import VvHero from './VvHero.svelte';
 </script>
 
 <VvHeader></VvHeader>
-
-<div class="m-8">
-	<Heading class="mb-4 text-center">Avance na vida.</Heading>
-	<P>Descubra como a Vanguard lhe põe à frente do mercado imobiliário.</P>
-</div>
 <VvHero></VvHero>
 <VvFooter></VvFooter>
