da1df9b7adf027af432791a9479e9defc9d4ab6b
Pedro Henrique Sauné <136375501+s4un3@users.noreply.github.com>
2025-01-03 12:40:58 +0000

    adicionar hr

--- Site/src/lib/VvFooter.svelte
+++ Site/src/lib/VvFooter.svelte
@@ -1,8 +1,9 @@
 <script>
-    import { Footer, P } from 'flowbite-svelte';
+    import { Footer, P, Hr } from 'flowbite-svelte';
 </script>
 
 <Footer class="flex flex-col items-center justify-center">
+    <Hr classHr="my-8 w-full h-1"/>
     <P>Imobiliária Vanguard</P>
     <P>Rua ??? - Cornélio Procópio, PR.</P>
 </Footer>
