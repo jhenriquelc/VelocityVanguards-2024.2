e641250477c71d191f73e7d0e58f2a4e7ae7b254
Pedro Henrique Sauné <136375501+s4un3@users.noreply.github.com>
2025-01-08 01:40:47 +0000

    criar um fale conosco

---
+++ Site/src/lib/VvContato.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { Heading, P } from "flowbite-svelte";
+</script>
+
+<div class="text-center">
+  <Heading tag="h2">Fale conosco</Heading>
+  <P class="text-center">Caso queira nos contatar fisicamente, venha ao nosso escritório. De segunda a sexta, das 6:00 às 20:00, na Avenida Alberto Carazzai, 1640.</P>
+  <P class="text-center">Número de telefone: (43) XXXX-XXXX.</P>
+  <P class="text-center">E-mail: imobiliariavanguards@gmail.com</P>
+</div>
--- Site/src/routes/contato/+page.svelte
+++ Site/src/routes/contato/+page.svelte
@@ -1,8 +1,11 @@
 <script>
 	import { P, Heading } from 'flowbite-svelte';
+	import VvContato from '$lib/VvContato.svelte'
 </script>
 
 <div class="m-8">
 	<Heading class="mb-4 text-center">Contato</Heading>
 	<P>Fale conosco</P>
 </div>
+
+<VvContato></VvContato>
