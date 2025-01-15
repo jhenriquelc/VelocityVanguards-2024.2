cc667f7fdf36bfc3dfefa835f861a7dec58b8c6c
Marcelo Angeli <marceloangeli@alunos.utfpr.edu.br>
2025-01-15 14:46:34 -0300

    Adicionar listagem dos imóveis cadastrados dentro no bd em uma tabela. Na tabela é possível clicar em deletar e editar (ainda não implementados)

--- Site/src/routes/admin/dashboard/+page.server.js
+++ Site/src/routes/admin/dashboard/+page.server.js
@@ -1,20 +1,20 @@
 import { redirect } from "@sveltejs/kit";
 import { ObterDados } from "$lib/db";
 
+
 export async function load({ cookies }){
+    const queryImoveis = `  SELECT 
+                            Imovel.ID_Imovel, 
+                            Imovel.Titulo, 
+                            Imovel.PrecoVenda, 
+                            Imovel.PrecoAluguel,
+                            Bairro.Nome AS BairroNome, 
+                            Rua.Nome AS RuaNome
+                            FROM Imovel
+                            JOIN Bairro ON Imovel.ID_Bairro = Bairro.ID_Bairro
+                            JOIN Rua ON Imovel.ID_Rua = Rua.ID_Rua
+                            `
 
-    let query = `SELECT * FROM Funcionario WHERE Cookie = ?`
-
-    let userCookiers = cookies.get('cookieLogin');
-
-    if( userCookiers === undefined){
-        userCookiers = ' ';
-    }
-    const imoveis = await ObterDados(query, userCookiers);
-    console.log(imoveis)
-
-    if(imoveis.length < 1){
-        redirect(301, '/admin');
-    }
-    return;
+    let imoveis = await ObterDados(queryImoveis);
+    return {imoveis}
 }
